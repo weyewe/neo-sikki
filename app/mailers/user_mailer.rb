c6b52b600ee37cb968fd5edc20a7ba39e8b63442
weyewe <w.yunnal@gmail.com>
2014-09-12 19:53:49 +0700

    awesome changes

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 # UserMailer.notify_new_user_registration( new_object , password    ).deliver
 
 class UserMailer < ActionMailer::Base
-  require 'fastercsv'
+  require 'csv'
   
   default from: "from@example.com"
 
@@ -15,23 +15,8 @@ class UserMailer < ActionMailer::Base
   end
   
   def sales_for_yesterday 
-    @from = 'someone@example.com' 
-    @recipients = 'w.yunnal@gmail.com' 
-    @sent_on = Time.now 
-    @yesterday = 1.day.ago 
-    @body = { :yesterday => @yesterday } 
-    @subject = "Sales Report"
-    
-    mail(:to => "admin@11ina.com", :subject => "banzai New account information")
-  
-    attachment :content_type => "text/csv", :filename => "sales_#{@yesterday.to_date}.csv" do |a| 
-      a.body = FasterCSV.generate do |csv| 
-        csv << (fields = ["artist", "product", "variant", "unit price", "qty sold", "total"]).map {|f| f.titleize } 
-        # Report.sales_for_date(@yesterday).each do |row| 
-        #   csv << fields.map {|f| row[f] } 
-        # end 
-      end 
-    end 
+    attachments["rails.png"] = File.read("#{Rails.root}/public/robots.txt")
+    mail(:to => "admin@11ina.com", :subject => "Registered")
   end
     
     
