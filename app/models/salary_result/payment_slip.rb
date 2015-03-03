31e55d4634c9bc74794c6aec294c8aa4c6f1c224
willy <w.yunnal@gmail.com>
2015-03-03 14:30:48 +0700

    fixed data generation

--- app/models/salary_result/payment_slip.rb
+++ app/models/salary_result/payment_slip.rb
@@ -76,23 +76,23 @@ module SalaryResult
           puts "absen: #{ x[9] }"
           element << x[9]
           puts "gaji pokok: #{self.extract_amount( x[10] )}"
-          element << self.extract_amount( x[10] )
+          element <<   x[10] 
           puts "tunjangan masa kerja: #{self.extract_amount( x[11] )}"
-          element << self.extract_amount( x[11] )
+          element <<   x[11] 
           puts "tunjangan hadir: #{self.extract_amount( x[12] )}"
-          element << self.extract_amount( x[12] )
+          element <<  x[12] 
           puts "tunjangan transport: #{self.extract_amount( x[13] )}"
-          element << self.extract_amount( x[13] )
+          element <<   x[13] 
           puts "tunjangan makan: #{self.extract_amount( x[14] )}"
-          element << self.extract_amount( x[14] )
+          element <<   x[14] 
 
           puts "potongan asuransi: #{self.extract_amount( x[15] )}"
-          element << self.extract_amount( x[15] )
+          element <<   x[15] 
           puts "potongan koperasi: #{self.extract_amount( x[16] )}"
-          element << self.extract_amount( x[16] )
+          element <<   x[16] 
 
           puts "Total: #{self.extract_amount( x[17] )}"
-          element << self.extract_amount( x[17] )
+          element <<  x[17] 
 
           result_array << element 
         end
