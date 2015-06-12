79129cee65f7cc20ee2267e044b7ecee92f79c06
willy <w.yunnal@gmail.com>
2015-06-12 19:48:07 +0700

    fixed the ordering. be specific by using table name

--- app/controllers/api2/membership_savings_reports_controller.rb
+++ app/controllers/api2/membership_savings_reports_controller.rb
@@ -5,14 +5,28 @@ class Api2::MembershipSavingsReportsController < Api2::BaseReportApiController
     client_starting_datetime   = params[:starting_datetime].to_datetime 
     client_ending_datetime = params[:ending_datetime].to_datetime 
 
-    Member.includes(:savings_entries).where{ savings_entries.is_confirmed.eq true }.count
+  #     today_kki_date = DateTime.now.in_time_zone 'Jakarta'
+  # last_year = today_kki_date - 1.years
+  # beginning_of_last_year = last_year.beginning_of_year
+  # ending_of_last_year = last_year.end_of_year
+  # starting_datetime = beginning_of_last_year.utc 
+  # ending_datetime = ending_of_last_year.utc 
+  #   client_starting_datetime = starting_datetime
+  #   client_ending_datetime =  ending_datetime
+
+
+
+    # @objects = Member.includes(:savings_entries).where(
+    #     :savings_entries => { :savings_status => SAVINGS_STATUS[:membership] }
+    #   ).page( 1 ).limit(  10) .order("members.id ASC")
+
 
     @objects = Member.includes(:savings_entries).where{
       ( savings_entries.savings_status.eq SAVINGS_STATUS[:membership] ) & 
       ( savings_entries.confirmed_at.lte client_ending_datetime ) & 
       ( savings_entries.confirmed_at.gte client_starting_datetime)
 
-    }.page( params[:page]).limit( params[:limit]).order("id ASC")
+    }.page( params[:page]).limit( params[:limit]).order("members.id ASC")
 
     @total = Member.includes(:savings_entries).where{
       ( savings_entries.savings_status.eq SAVINGS_STATUS[:membership] ) & 
@@ -24,22 +38,6 @@ class Api2::MembershipSavingsReportsController < Api2::BaseReportApiController
   end
 
 end
-
-
-  # json.id_number            object.id_number
-  # json.name             object.name
-
-
-  # json.membership_savings_entries object.savings_entries.where(
-  #               :savings_status => SAVINGS_STATUS[:membership],
-  #               :is_confirmed => true ) do |membership_savings|
-
-  #   json.id         membership_savings.id 
-  #   json.direction      membership_savings.direction
-  #   json.amount       membership_savings.amount 
-
-  #   json.confirmed_at   format_date_friendly( membership_savings.confirmed_at )
-  # end
 
 
 
--- app/views/api2/membership_savings_reports/index.json.jbuilder
+++ app/views/api2/membership_savings_reports/index.json.jbuilder
@@ -1,7 +1,26 @@
 json.success true 
 json.total @total
-json.meongs @objects do |object|
+json.members @objects do |object|
 	json.id 								object.id 
+
+
+
+
+  json.id_number            object.id_number
+  json.name             object.name
+
+
+  json.membership_savings_entries object.savings_entries.where(
+                :savings_status => SAVINGS_STATUS[:membership],
+                :is_confirmed => true ) do |membership_savings|
+
+    json.id         membership_savings.id 
+    json.direction      membership_savings.direction
+    json.amount       membership_savings.amount 
+
+    json.confirmed_at   format_date_friendly( membership_savings.confirmed_at )
+  end
+
 
 
 
