86c92b4bf69200367da08b316fe76988e89e9335
willy <w.yunnal@gmail.com>
2015-06-12 19:11:09 +0700

    convert from jonis to includes

--- app/controllers/api2/membership_savings_reports_controller.rb
+++ app/controllers/api2/membership_savings_reports_controller.rb
@@ -5,14 +5,14 @@ class Api2::MembershipSavingsReportsController < Api2::BaseReportApiController
     client_starting_datetime   = params[:starting_datetime].to_datetime 
     client_ending_datetime = params[:ending_datetime].to_datetime 
 
-    @objects = Member.joins(:savings_entries).where{
+    @objects = Member.includes(:savings_entries).where{
       ( savings_entries.savings_status == SAVINGS_STATUS[:membership] ) & 
       ( savings_entries.confirmed_at.lte client_ending_datetime ) & 
       ( savings_entries.confirmed_at.gte client_starting_datetime)
 
     }.page( params[:page]).limit( params[:limit]).order("id ASC")
 
-    @total = Member.joins(:savings_entries).where{
+    @total = Member.includes(:savings_entries).where{
       ( savings_entries.savings_status == SAVINGS_STATUS[:membership] ) & 
       ( savings_entries.confirmed_at.lte client_ending_datetime ) & 
       ( savings_entries.confirmed_at.gte client_starting_datetime)
