33b0daf750e1416de378ada8cb11a4990016397c
willy <w.yunnal@gmail.com>
2015-06-12 09:28:04 +0700

    join instead of includes

--- app/controllers/api2/membership_savings_reports_controller.rb
+++ app/controllers/api2/membership_savings_reports_controller.rb
@@ -5,14 +5,14 @@ class Api2::MembershipSavingsReportsController < Api2::BaseReportApiController
     client_starting_datetime   = params[:starting_datetime].to_datetime 
     client_ending_datetime = params[:ending_datetime].to_datetime 
 
-    @objects = Member.includes(:savings_entries).where{
+    @objects = Member.joins(:savings_entries).where{
       ( savings_entries.savings_status == SAVINGS_STATUS[:membership] ) & 
       ( savings_entries.confirmed_at.lte client_ending_datetime ) & 
       ( savings_entries.confirmed_at.gte client_starting_datetime)
 
     }.page( params[:page]).limit( params[:limit]).order("id ASC")
 
-    @total = Member.includes(:savings_entries).where{
+    @total = Member.joins(:savings_entries).where{
       ( savings_entries.savings_status == SAVINGS_STATUS[:membership] ) & 
       ( savings_entries.confirmed_at.lte client_ending_datetime ) & 
       ( savings_entries.confirmed_at.gte client_starting_datetime)
@@ -22,3 +22,15 @@ class Api2::MembershipSavingsReportsController < Api2::BaseReportApiController
   end
 
 end
+
+
+
+  # json.membership_savings_entries object.savings_entries.where(
+  #               :savings_status => SAVINGS_STATUS[:membership],
+  #               :is_confirmed => true ) do |membership_savings|
+
+  #    json.direction  membership_savings.direction
+  #    json.amount membership_savings.amount 
+
+  #   json.confirmed_at   format_date_friendly( membership_savings.confirmed_at )
+  # end
